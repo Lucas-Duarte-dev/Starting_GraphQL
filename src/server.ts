ef6335cc24f556cff48c5dc018caf88eeda77344
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-06-29 22:25:15 -0300

    Create User

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { ApolloServer, gql } from "apollo-server";
 
+import { v4 as uuid } from "uuid";
+
 // Toda request é POST!!
 
 // Toda requisição bate no mesmo endpoint (/graphql)
@@ -9,13 +11,56 @@ import { ApolloServer, gql } from "apollo-server";
 // Scalar Types -> String, Int, Boolean, Float e ID (Tipos primitivos, tirando o ID que é um tipo primitivo do
 // do Graphql)
 
-const typeDefs = gql`
-  type Query {
+// O "!" serve para dizer que tal valor é obrigatório
 
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
+  type Query {
+    hello: String
+    users: [User!]!
   }
 `;
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    hello: () => "Hello World",
+
+    users: () => [
+      {
+        _id: uuid(),
+        name: "Lucas Duarte",
+        email: "lucas.duarte@gmail.com",
+        active: true,
+      },
+      {
+        _id: uuid(),
+        name: "Lucas Duarte1",
+        email: "lucas.duarte@gmail.com",
+        active: false,
+      },
+      {
+        _id: uuid(),
+        name: "Lucas Duarte2",
+        email: "lucas.duarte@gmail.com",
+        active: false,
+      },
+    ],
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
+
 server.listen().then(({ url }) => console.log(`Running ${url}`));
