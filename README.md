416e9f51ffffb93237635c4a503f5aacd171bdf1
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-01 19:38:45 -0300

    Refactory modules

---
+++ README.md
@@ -0,0 +1,13 @@
+# Entendendo o GraphQL
+
+## Pontos importantes
+
+- Toda request é POST!!
+
+- Toda requisição bate no mesmo endpoint (/graphql)
+
+- Query -> Obter informações (GET)
+- Mutation -> Manipular dados (POST/PUT/PATCH/DELETE)
+- Scalar Types -> String, Int, Boolean, Float e ID (Tipos primitivos, tirando o ID que é um tipo primitivo do Graphql)
+
+- O "!" serve para dizer que tal valor é obrigatório
--- src/graphql/modules/users/resolvers.ts
+++ src/graphql/modules/users/resolvers.ts
@@ -1,4 +1,14 @@
+import User from "../../../entities/User";
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    users: () => User.find(),
+    user: (_, { id }) => User.findById(id),
+  },
+  Mutation: {
+    createUser: (_, { data }) => User.create(data),
+    updateUser: (_, { id, data }) =>
+      User.findOneAndUpdate(id, data, { new: true }),
+    deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
+  },
 };
