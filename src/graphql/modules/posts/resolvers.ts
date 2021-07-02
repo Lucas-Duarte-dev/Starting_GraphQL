5d98255d594a468c4ecfcf26722d5e6d3049a88f
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-02 20:09:32 -0300

    Context GraphQL

--- src/graphql/modules/posts/resolvers.ts
+++ src/graphql/modules/posts/resolvers.ts
@@ -1,6 +1,11 @@
 import Post from "../../../entities/Post";
+import User from "../../../entities/User";
 
 export default {
+  Post: {
+    author: (post) => User.findById(post.author),
+  },
+
   Query: {
     posts: () => Post.find(),
     post: (_, { id }) => Post.findById(id),
